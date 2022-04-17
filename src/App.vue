dfa1d435cb3c35c6bdd6da29237a4c589449be13
L1brA7 <umashev58@gmail.com>
2022-04-17 13:01:46 +0300

    develop init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TopNavBar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNavBar from './components/TopNavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopNavBar
   }
 }
 </script>
@@ -20,7 +19,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgb(230, 230, 230)
 }
 </style>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top-navbar">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNavBar',
+}
+</script>
+
+<style scoped>
+  top-navbar {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    background-color: #777777
+  }
+</style>
